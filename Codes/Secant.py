a9ca68de3a546d8f785ccb703f0362b8fa67e6cd
tishat-ahasan <tishat.ahasan004@gmail.com>
2019-11-08 20:36:12 +0600

    Secant Method

--- Codes/Secant.py
+++ Codes/Secant.py
@@ -1,4 +1,4 @@
-#finding the root of x^2-x-2 in
+#finding the root of x^2-4x-10=0 in
 # the rang of 1<x<3 using
 #false Position Method.
 import matplotlib.pyplot as plt
@@ -7,9 +7,9 @@ import numpy as np
 
 
 def f(x):
-    return x**2-x-2
+    return x**2-4*x-10
 
-x = np.linspace(-5,5,100)
+x = np.linspace(-5,10,100)
 y = f(x)
 
 
@@ -17,24 +17,21 @@ y = f(x)
 
 #we use 100 iteration
 
-x1,x2 = -0.5,3
+x1,x2 = 4,2  #initial estimation
 for i in range(0,10):
-    plt.plot(x, y, label='x^2-x-2=0')
-    x0 = x1 - f(x1)*((x2-x1)/(f(x2)-f(x1)))
+    x3 = ((f(x2)*x1) - (f(x1)*x2))/(f(x2)-f(x1))
     plt.plot((x1, x1), (0, f(x1)),label='point X1')
     plt.plot((x2, x2), (0, f(x2)),label='point X2')
-    plt.plot((x1,x2),(f(x1),f(x2)))
-    if f(x0)*f(x1)<0:
-        x2 = x0
-    else:
-        x1 = x0
+    #plt.plot((x1,x2),(f(x1),f(x2)))
+    x1 = x2
+    x2 = x3
 
     plt.plot((-10, 10), (0, 0), 'r--')
-    plt.plot((0, 0),(-10, 10) ,'r--')
-    plt.plot((x0, x0), (0, f(x0)),label='point X0')
+    plt.plot((0, 0), (min(y)-2, max(y)+2), 'r--')
+    plt.plot((x3, x3), (0, f(x3)),label='point X3')
 
-
-    plt.ylim(-5, 10)
+    plt.plot(x, y,label='x^2-4x-10=0')
+    #plt.ylim(-5, 10)
     plt.legend()
     plt.show()
 
--- README.md
+++ README.md
@@ -7,3 +7,7 @@
  
  >False Position Method
  >>finding the root of x^2-x-2 in the rang of 1<x<3 using false Position Method.
+
+>Secant Method
+>> finding the root of x^2-4x-10 in the rang of 2<x<4 using false Position Method.
+
